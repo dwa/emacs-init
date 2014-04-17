23c6e0dad27149b022d6db7535d07644d886a53c
David Wallin <david.wallin@seamless.se>
2014-04-17 16:06:44 +0200

    custom++

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -1,7 +1,7 @@
-;;; custom.el - 
-;;; 
+;;; custom.el -
 ;;;
-;;; Time-stamp: <2012-08-28 12:18:07 dwa>
+;;;
+;;; Time-stamp: <2014-03-06 13:27:29 dwa>
 ;;;
 ;;; copyright (c) 2008. all rights reserved.
 ;;; David Wallin <dwa@havanaclub.org>
@@ -17,17 +17,21 @@
 (put 'base 'safe-local-variable 'integerp)
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(load-home-init-file t t))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(load-home-init-file t t)
+ '(safe-local-variable-values (quote ((flycheck-checker quote python-pylint) (flycheck-checker quote python-flake8) (flycheck-checker . "python-flake8") (python-shell-interpreter-args . "-m IPython.terminal.ipapp") (python-shell-interpreter . "python") (python-shell-completion-string-code . "';'.join(get_ipython().Completer.all_completions('''%s'''))
+") (python-shell-completion-module-string-code . "';'.join(module_completion('''%s'''))
+") (python-shell-completion-setup-code . "from IPython.core.completerlib import module_completion") (python-shell-interpreter-args . "") (python-shell-interpreter . "ipython"))))
+ '(session-use-package t nil (session)))
 
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
 
 (put 'narrow-to-region 'disabled nil)
