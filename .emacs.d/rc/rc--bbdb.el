9b45620a4234d43d4d9a6489a414cc232cddcd7d
David Wallin <david.wallin@seamless.se>
2015-08-02 23:58:55 +0200

    bbdb -> use-package

--- .emacs.d/rc/rc--bbdb.el
+++ .emacs.d/rc/rc--bbdb.el
@@ -1,49 +1,54 @@
-;;; Time-stamp: <2013-04-14 00:25:07 dwa>
+;;; Time-stamp: <2015-08-02 02:51:16 davidwallin>
 
 
 ;;; Code:
 
-;; (require 'bbdb)
-(require 'bbdb-snarf)
-;; (bbdb-initialize)
-
-(eval-after-load "gnus"
-  '(bbdb-insinuate-gnus))
-
-;; enable pop-ups
-(setq bbdb-use-pop-up nil)
-
-;; auto collection
-(setq bbdb/mail-auto-create-p nil)
-
-;; exceptional folders against auto collection
-(setq bbdb-wl-ignore-folder-regexp "^@")
-(setq signature-use-bbdb t)
-(setq bbdb-north-american-phone-numbers-p nil)
-;; automatically add mailing list fields
-(add-hook 'bbdb-notice-hook 'bbdb-auto-notes-hook)
-(setq bbdb-auto-notes-alist '(("X-ML-Name" (".*$" ML 0))))
-
-(setq bbdb-complete-name-allow-cycling t)
-(setq bbdb-expand-mail-aliases t)
-(setq bbdb-use-alternate-names nil)
-(setq bbdb-quiet-about-name-mismatches t)
-(setq bbdb-display-layout 'one-line)
+(use-package bbdb
+  :config
 
 
-(eval-after-load "bbdb-com"
-  '(load "bbdb-patches"))
+  (require 'bbdb-snarf)
+  ;; (bbdb-initialize)
 
-;;moy-bbdb notices outgoing adresses
-(autoload 'bbdb/send-hook "moy-bbdb"
-  "Function to be added to `message-send-hook' to notice records when sending messages" t)
-;;(add-hook 'message-send-hook 'bbdb/send-hook) ; If you use Gnus
+  (eval-after-load "gnus"
+    '(bbdb-insinuate-gnus))
 
-;;To have this automatically add outgoing addresses, use this.
-;;(setq bbdb/send-auto-create-p t)
-;;(setq bbdb/send-prompt-for-create-p nil)
+  ;; enable pop-ups
+  (setq bbdb-use-pop-up nil)
 
-;;To query whether to add outgoing messages, use this.
-;; (setq bbdb/send-auto-create-p 'prompt)
+  ;; auto collection
+  (setq bbdb/mail-auto-create-p nil)
+
+  ;; exceptional folders against auto collection
+  (setq bbdb-wl-ignore-folder-regexp "^@")
+  (setq signature-use-bbdb t)
+  (setq bbdb-north-american-phone-numbers-p nil)
+  ;; automatically add mailing list fields
+  (add-hook 'bbdb-notice-hook 'bbdb-auto-notes-hook)
+  (setq bbdb-auto-notes-alist '(("X-ML-Name" (".*$" ML 0))))
+
+  (setq bbdb-complete-name-allow-cycling t)
+  (setq bbdb-expand-mail-aliases t)
+  (setq bbdb-use-alternate-names nil)
+  (setq bbdb-quiet-about-name-mismatches t)
+  (setq bbdb-display-layout 'one-line)
+
+  (eval-after-load "bbdb-com"
+    '(load "bbdb-patches"))
+
+  ;;moy-bbdb notices outgoing adresses
+  (autoload 'bbdb/send-hook "moy-bbdb"
+    "Function to be added to `message-send-hook' to notice records when sending messages" t)
+
+  ;;(add-hook 'message-send-hook 'bbdb/send-hook) ; If you use Gnus
+
+  ;;To have this automatically add outgoing addresses, use this.
+  ;;(setq bbdb/send-auto-create-p t)
+  ;;(setq bbdb/send-prompt-for-create-p nil)
+
+  ;;To query whether to add outgoing messages, use this.
+  ;; (setq bbdb/send-auto-create-p 'prompt)
+
+  :ensure t)
 
 ;;; ----------------------------------------------------------------- [the end]
