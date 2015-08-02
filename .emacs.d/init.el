40a11fb7a2231935ea661892b22ce13a9f794ce2
David Wallin <david.wallin@seamless.se>
2015-08-02 23:59:32 +0200

    move alot of packages over to use-package
    (have to find better places for them later)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
 ;;; -*- emacs-lisp -*-
-;;; Time-stamp: <2015-07-30 15:04:04 davidwallin>
+;;; Time-stamp: <2015-08-02 23:29:08 davidwallin>
 ;;; David Wallin <dwa@havanaclub.org>
 
 ;;; Code:
@@ -88,8 +88,123 @@
   (setq yas/trigger-key "S-SPC")
   (yas-global-mode 1))
 
-;; (use-package auto-complete-yasnippet
-;;   :ensure t)
+(use-package auto-complete
+  :init
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+  :config
+  (require 'auto-complete)
+  (require 'auto-complete-config)
+  (ac-config-default)
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+  (add-to-list 'ac-sources 'ac-source-yasnippet))
+
+(use-package cedet
+  :init
+  (setq semanticdb-default-save-directory (expand-file-name "~/.emacs.d/.semanticdb")
+        semanticdb-persistent-path
+        (list (expand-file-name "~/.emacs.d/semantic-cache")))
+  :config
+  (add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode t)
+  (add-to-list 'semantic-default-submodes 'global-semantic-highlight-func-mode t)
+  (add-to-list 'semantic-default-submodes 'global-semantic-show-unmatched-syntax-mode t)
+
+  (add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode t)
+  (add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode t)
+
+  (global-ede-mode 1)
+
+  (semantic-load-enable-code-helpers)
+  (global-semanticdb-minor-mode 1))
+
+(use-package session
+  :init
+  (add-hook 'after-init-hook 'session-initialize)
+  :ensure t)
+
+(use-package smartparens
+  :init
+  :config
+  (require 'smartparens)
+  (require 'smartparens-config)
+  (setq sp-highlight-pair-overlay nil)
+  (sp-use-paredit-bindings)
+  (define-key sp-keymap (kbd ")") 'sp-up-sexp)
+  (smartparens-global-mode t)
+  (show-smartparens-global-mode t)
+  :ensure t)
+
+(use-package ack-menu
+  :ensure t)
+
+(use-package ansible-doc
+  :ensure t)
+
+(use-package yaml-mode
+  :init
+  (add-hook 'yaml-mode-hook #'ansible-doc-mode)
+  :ensure t)
+
+(use-package dictionary
+  :commands (dictionary-search dictionary-match-words)
+  :init
+  (bind-key "\C-cs" 'dictionary-search)
+  (bind-key "\C-cm" 'dictionary-match-words)
+  :ensure t)
+
+(use-package color-moccur
+  :commands (isearch-moccur isearch-all)
+  :bind ("M-s O" . moccur)
+  :init
+  (bind-key "M-o" 'isearch-moccur isearch-mode-map)
+  (bind-key "M-O" 'isearch-moccur-all isearch-mode-map)
+;  :config
+;  (use-package moccur-edit :ensure t)
+  :ensure t)
+
+(use-package flycheck
+  :config
+  (defun my-flycheck-keybindings ()
+    (interactive)
+    (local-set-key (kbd "C-c C-k") 'flycheck-buffer)
+    (local-set-key (kbd "M-n") 'next-error)
+    (local-set-key (kbd "M-p") 'previous-error))
+
+  (add-hook 'flycheck-mode-hook
+            #'my-flycheck-keybindings)
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+  :ensure t)
+
+
+(use-package lentic
+  :ensure t)
+
+(use-package lacarte
+  :ensure t)
+
+(use-package multiple-cursors
+  :ensure t)
+
+(use-package request-deferred
+  :ensure t)
+
+(use-package rainbow-delimiters
+  :ensure t)
+
+;; docker
+(use-package docker
+  :ensure t)
+
+;(use-package docker-tramp
+;  :ensure t)
+
+(use-package dockerfile-mode
+  :mode "Dockerfile"
+  :ensure t)
+
+;; SQL
+
+(use-package edbi
+  :ensure t)
 
 ;;; --------------------------------------------------- [line ending conversion]
 
--- .emacs.d/rc/rc--el-get.el
+++ .emacs.d/rc/rc--el-get.el
@@ -1,4 +1,4 @@
-;;; Time-stamp: <2015-07-30 15:30:38 davidwallin>
+;;; Time-stamp: <2015-08-02 23:11:29 davidwallin>
 
 ;;; Code:
 
@@ -11,55 +11,27 @@
       (end-of-buffer)
       (eval-print-last-sexp))))
 
-
 (add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
 (setq el-get-sources
       '(el-get
-        (:name ack-menu)
-        (:name ansible-doc)
-        (:name auto-complete
-               :after (progn
-			(require 'auto-complete)
-                        (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-			(require 'auto-complete-config)
-                        (ac-config-default)
-                        (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-
-                        (add-to-list 'ac-sources 'ac-source-yasnippet)))
-        (:name auto-complete-clang
-               :after (progn
-                        (require 'auto-complete-clang)
-                        (setq ac-clang-flags
-                              (mapcar (lambda (item)(concat "-I" item))
-                                      (split-string
-                                       (shell-command-to-string
-                                        "echo \"\" | g++ -v -x c++ -E - 2>&1| awk '/^#include <...>/,/^End/'|awk '/^ /{ print; }'"))))
-                        ;; we don't want to use clang with Java:
-                        ;; (add-hook 'c-mode-common-hook
-                        ;;           '(lambda ()
-                        ;;              (add-to-list 'ac-sources 'ac-source-clang)))
-                        (add-hook 'c-mode-hook
-                                  '(lambda ()
-                                     (add-to-list 'ac-sources 'ac-source-clang)))
-                        (add-hook 'c++-mode-hook
-                                  '(lambda ()
-                                     (add-to-list 'ac-sources 'ac-source-clang)))))
-        (:name cedet
-               :after (progn
-                        (setq semanticdb-default-save-directory (expand-file-name "~/.emacs.d/.semanticdb")
-                              semanticdb-persistent-path
-                              (list (expand-file-name "~/.emacs.d/semantic-cache")))
-
-                        (add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode t)
-                        (add-to-list 'semantic-default-submodes 'global-semantic-highlight-func-mode t)
-                        (add-to-list 'semantic-default-submodes 'global-semantic-show-unmatched-syntax-mode t)
-
-                        (add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode t)
-                        (add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode t)
-                        (global-ede-mode 1)
-
-                        (semantic-load-enable-code-helpers)
-                        (global-semanticdb-minor-mode 1)))
+        ;; (:name auto-complete-clang
+        ;;        :after (progn
+        ;;                 (require 'auto-complete-clang)
+        ;;                 (setq ac-clang-flags
+        ;;                       (mapcar (lambda (item)(concat "-I" item))
+        ;;                               (split-string
+        ;;                                (shell-command-to-string
+        ;;                                 "echo \"\" | g++ -v -x c++ -E - 2>&1| awk '/^#include <...>/,/^End/'|awk '/^ /{ print; }'"))))
+        ;;                 ;; we don't want to use clang with Java:
+        ;;                 ;; (add-hook 'c-mode-common-hook
+        ;;                 ;;           '(lambda ()
+        ;;                 ;;              (add-to-list 'ac-sources 'ac-source-clang)))
+        ;;                 (add-hook 'c-mode-hook
+        ;;                           '(lambda ()
+        ;;                              (add-to-list 'ac-sources 'ac-source-clang)))
+        ;;                 (add-hook 'c++-mode-hook
+        ;;                           '(lambda ()
+        ;;                              (add-to-list 'ac-sources 'ac-source-clang)))))
         (:name coffee-mode
                :after (progn (setq coffee-command
                                    "~/.npm/coffee-script/1.3.3/package/bin/coffee")
@@ -76,26 +48,19 @@
                                    coffeelintnode-coffeelint-set ""
                                    ;; Start the server when we first open a coffee file and start checking
                                    coffeelintnode-autostart 'true)))
-        clojure-mode
-        swank-clojure
-        color-moccur
-        moccur-edit
-        dbgr
-        (:name dictionary
-               :after (progn
-                        ;(setq dictionary-server "localhost")
-                        (global-set-key "\C-cs" 'dictionary-search)
-                        (global-set-key "\C-cm" 'dictionary-match-words)))
-        (:name ein
-               :after (progn
-			(require 'ein)
-                        (setq ein:use-auto-complete-superpack t
-                              ein:propagate-connect t)
-                        (setq ein:connect-default-notebook "88888/SeamlessNotebook")
-                        (add-hook 'python-mode-hook 'ein:connect-to-default-notebook)
-                        (add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
-                        ;; load notebook list if Emacs is idle for 3 sec after start-up
-                        (run-with-idle-timer 3 nil #'ein:notebooklist-load)))
+        ;; clojure-mode
+        ;; swank-clojure
+        ;; dbgr
+        ;; (:name ein
+        ;;        :after (progn
+	;; 		(require 'ein)
+        ;;                 (setq ein:use-auto-complete-superpack t
+        ;;                       ein:propagate-connect t)
+        ;;                 (setq ein:connect-default-notebook "88888/SeamlessNotebook")
+        ;;                 (add-hook 'python-mode-hook 'ein:connect-to-default-notebook)
+        ;;                 (add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
+        ;;                 ;; load notebook list if Emacs is idle for 3 sec after start-up
+        ;;                 (run-with-idle-timer 3 nil #'ein:notebooklist-load)))
         erc-extras
         gnus
         google-maps
@@ -121,9 +86,6 @@
                               (when (string-match ".x?html$")
                                 (nxml-html-outline)))
                             (add-hook 'nxml-mode-hook 'my-nxml-hook)))))
-        (:name lacarte
-               :after (progn (require 'lacarte)))
-        (:name lentic)
         (:name js2
                :after (progn (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
                              (add-hook 'js2-mode-hook
@@ -131,38 +93,7 @@
                                           (local-set-key (kbd "M-n") 'next-error)
                                           (local-set-key (kbd "M-p") 'previous-error)))))
         offlineimap
-        (:name parenface
-               :after (progn
-                        ;;
-                        ;; the brackets gets invisible on the terminal with parenface:
-                        (when t ;window-system
-                          (require 'parenface))))
-        (:name flycheck :after (progn (defun my-flycheck-keybindings ()
-                                        (interactive)
-                                        (local-set-key (kbd "C-c C-k") 'flycheck-buffer)
-                                        (local-set-key (kbd "M-n") 'next-error)
-                                        (local-set-key (kbd "M-p") 'previous-error))
 
-                                      (add-hook 'flycheck-mode-hook
-                                                #'my-flycheck-keybindings)
-                                      (add-hook 'after-init-hook #'global-flycheck-mode)))
-        (:name session
-	       :after (progn (add-hook 'after-init-hook 'session-initialize)))
-        (:name smartparens
-               :after (progn
-                        (require 'smartparens)
-                        (require 'smartparens-config)
-                        (setq sp-highlight-pair-overlay nil)
-                        (sp-use-paredit-bindings)
-                        (define-key sp-keymap (kbd ")") 'sp-up-sexp)
-                        (smartparens-global-mode t)
-                        (show-smartparens-global-mode t)))
-        ;; (:name smex
-        ;;        :after (progn
-        ;;                 (global-set-key (kbd "M-x") 'smex)
-        ;;                 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-        ;;                 ;; This is your old M-x.
-        ;;                 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)))
         (:name visual-regexp)
         (:name visual-regexp-steroids
                :after (progn (require 'visual-regexp-steroids)
@@ -174,13 +105,6 @@
                              (define-key esc-map (kbd "C-r") 'vr/isearch-backward) ;; C-M-r
                              (define-key esc-map (kbd "C-s") 'vr/isearch-forward) ;; C-M-s
                              ))
-        (:name yaml-mode
-               :after (progn
-                        (add-hook 'yaml-mode-hook #'ansible-doc-mode)))
-        (:name auto-complete-yasnippet)
-        ;; (:name yasnippet
-        ;;        :after (progn (setq yas/trigger-key "S-SPC")
-        ;;                      (yas-global-mode 1)))
         ))
 (el-get 'sync)
 
