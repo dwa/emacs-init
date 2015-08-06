cff0932ebbaf1b10de7f8519d559c7b23de32560
David Wallin <david.wallin@seamless.se>
2015-08-06 15:48:25 +0200

    use-packageify flymake config

--- .emacs.d/rc/rc--flymake.el
+++ .emacs.d/rc/rc--flymake.el
@@ -1,54 +1,42 @@
-;;; Time-stamp: <2013-04-14 00:25:06 dwa>
-
-
+;;; Time-stamp: <2015-08-06 15:40:41 davidwallin>
 ;;; Code:
 
-;; '(flymake-errline ((((class color)) (:underline "OrangeRed"))))
-;; '(flymake-warnline ((((class color)) (:underline "yellow"))))
+(use-package flymake
+  :commands (flymake-find-file-hook)
+  :init
+  (setq flymake-gui-warnings-enabled nil)
+  (setq flymake-log-level 1)
 
-(require 'flymake)
+  ;; Instead of manually activating flymake-mode, you can configure Flymake to
+  ;; automatically enable flymake-mode upon opening any file for which syntax
+  ;; check is possible. To do so, place the following line in .emacs:
+  (add-hook 'find-file-hook 'flymake-find-file-hook)
 
-(setq flymake-gui-warnings-enabled nil)
-(setq flymake-log-level 1)
+  (defun my-flymake-show-next-error ()
+    (interactive)
+    (flymake-goto-next-error)
+    (when flymake-gui-warnings-enabled
+      (flymake-display-err-menu-for-current-line)))
 
-;; Instead of manually activating flymake-mode, you can configure Flymake to
-;; automatically enable flymake-mode upon opening any file for which syntax
-;; check is possible. To do so, place the following line in .emacs:
-(add-hook 'find-file-hook 'flymake-find-file-hook)
+  (defun my-flymake-show-prev-error ()
+    (interactive)
+    (flymake-goto-prev-error)
+    (when flymake-gui-warnings-enabled
+      (flymake-display-err-menu-for-current-line)))
 
-(eval-after-load "flymake"
-  '(defun flymake-get-tex-args (file-name)
+  (defun my-flymake-keybindings ()
+    (interactive)
+    ;; (local-set-key (kbd "C-c C-k") 'compile)
+    (local-set-key (kbd "C-c C-k") 'flymake-compile)
+    (local-set-key (kbd "M-n") 'my-flymake-show-next-error)
+    (local-set-key (kbd "M-p") 'my-flymake-show-prev-error))
+  :config
+  (defun flymake-get-tex-args (file-name)
      (list "chktex" (list "-q" "-v0" file-name)))
   ;; '(defun flymake-get-tex-args (file-name)
   ;;   (list "pdflatex" (list "-file-line-error" "-draftmode" "-interaction=nonstopmode" file-name)))
-  )
 
-(setq flymake-allowed-file-name-masks
-      (remove-if (lambda (x) (string-match ".*\\(\\\\.tex\\|\\\\.xml\\|\\\\.html\\?\\)\\\\'" (first x)))
-                 flymake-allowed-file-name-masks))
-
-
-;; REMEMBER: Makefile rule 'check-syntax'
-;; (add-to-list 'flymake-allowed-file-name-masks
-;; 	     '( "\\.cc$" flymake-simple-make-init))
-
-(defun my-flymake-show-next-error ()
-  (interactive)
-  (flymake-goto-next-error)
-  (when flymake-gui-warnings-enabled
-    (flymake-display-err-menu-for-current-line)))
-
-
-(defun my-flymake-show-prev-error ()
-  (interactive)
-  (flymake-goto-prev-error)
-  (when flymake-gui-warnings-enabled
-    (flymake-display-err-menu-for-current-line)))
-
-
-(defun my-flymake-keybindings ()
-  (interactive)
-  ;; (local-set-key (kbd "C-c C-k") 'compile)
-  (local-set-key (kbd "C-c C-k") 'flymake-compile)
-  (local-set-key (kbd "M-n") 'my-flymake-show-next-error)
-  (local-set-key (kbd "M-p") 'my-flymake-show-prev-error))
+  (setq flymake-allowed-file-name-masks
+        (remove-if (lambda (x) (string-match ".*\\(\\\\.tex\\|\\\\.xml\\|\\\\.html\\?\\)\\\\'" (first x)))
+                   flymake-allowed-file-name-masks))
+  :ensure t)
