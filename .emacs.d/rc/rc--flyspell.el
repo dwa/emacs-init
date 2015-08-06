7a0578beaad7965fb813db1644b2ac73461164e1
David Wallin <david.wallin@seamless.se>
2015-08-06 15:48:05 +0200

    use-packageify flyspell config

--- .emacs.d/rc/rc--flyspell.el
+++ .emacs.d/rc/rc--flyspell.el
@@ -1,28 +1,27 @@
-;;; Time-stamp: <2013-04-14 00:25:06 dwa>
-
-
+;;; Time-stamp: <2015-08-06 15:46:05 davidwallin>
 ;;; Code:
 
-(setq-default ispell-program-name "aspell")
+(use-package flyspell
+  :commands (flyspell turn-on-flyspell tex-mode-flyspell-verify)
+  :init
+  (setq-default ispell-program-name "aspell")
+  (setq ispell-local-dictionary "english")
 
-(setq ispell-local-dictionary "english")
+  ;; (autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
+  ;; (autoload 'flyspell-delay-command "flyspell" "Delay on command." t)
+  ;; (autoload 'tex-mode-flyspell-verify "flyspell" "" t)
+  :config
+  ;; prefer to have M-TAB for other things:
+  (define-key flyspell-mode-map (kbd "M-TAB") nil)
 
-(autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
-(autoload 'flyspell-delay-command "flyspell" "Delay on command." t)
-(autoload 'tex-mode-flyspell-verify "flyspell" "" t)
+  ;; the default flyspell behaviour
+  (put 'LaTeX-mode 'flyspell-mode-predicate 'tex-mode-flyspell-verify)
 
-;;; prefer to have M-TAB for other things:
-(eval-after-load "flyspell"
-  '(define-key flyspell-mode-map (kbd "M-TAB") nil))
+  ;; some extra flyspell delayed command
+  (mapcar 'flyspell-delay-command '(scroll-up1 scroll-down1))
 
-
-;; the default flyspell behaviour
-(put 'LaTeX-mode 'flyspell-mode-predicate 'tex-mode-flyspell-verify)
-
-;; some extra flyspell delayed command
-(mapcar 'flyspell-delay-command '(scroll-up1 scroll-down1))
-
-(add-hook 'LaTeX-mode-hook (lambda () (setq ispell-parser 'tex)))
-(add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
+  (add-hook 'LaTeX-mode-hook (lambda () (setq ispell-parser 'tex)))
+  (add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
+  :ensure t)
 
 ;;; ----------------------------------------------------------------- [the end]
