1c86539040070a91321f10029a3c204d11200c39
David Wallin <david.wallin@seamless.se>
2015-08-08 03:39:46 +0200

    remove some darwin related cruft

--- .emacs.d/rc/rc--darwin.el
+++ .emacs.d/rc/rc--darwin.el
@@ -1,22 +1,13 @@
-;;; Time-stamp: <2015-07-15 21:39:53 davidwallin>
-
-
+;;; Time-stamp: <2015-08-08 01:29:25 davidwallin>
 ;;; Code:
 
 (when (eq system-type 'darwin)
-
-  (setq mac-command-key-is-meta t)
-
   ;;
   ;; untranslate some keypad keys:
   (mapc (lambda (x)
 	  (define-key function-key-map (vector x) nil))
 	'(kp-0 kp-1 kp-2 kp-3 kp-4 kp-5 kp-6 kp-7 kp-8 kp-9))
 
-;  (setenv "SSH_AUTH_SOCK" "/tmp/501/SSHKeychain.socket")
-
-  (setenv "PATH" (concat (expand-file-name "~/bin")
-                         ":/usr/local/bin:" (getenv "PATH")))
   ;;
   ;; Replace the xpdf as View command (with 'open') on the mac:
   (defun redefine-auctex-pdf-view-style ()
